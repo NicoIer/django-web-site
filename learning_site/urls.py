944dc091ac9013c97384068f9ae9572f83e6fc9b
ChenJun <NicoIer@163.com>
2022-09-14 21:35:17 +0800

    小打小闹的app01测试app

--- learning_site/urls.py
+++ learning_site/urls.py
@@ -20,6 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('',include('web.urls')),
-    path('app01/', include('app01.urls'))
+    # path('',include('web.urls')),
+    path('app01/', include('app01.urls')),
 ]
---
+++ web/views/account.py
@@ -0,0 +1,7 @@
+from django_redis import get_redis_connection
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+
+
+def register(request):
+    # return render(request)
+    pass
