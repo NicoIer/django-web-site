b1bdfca40712fb0eb282342a1c635c26ed99ed96
ChenJun <NicoIer@163.com>
2022-09-17 13:37:12 +0800

    自定义了用户登录检查的装饰器

---
+++ web/decorators.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+
+
+def check_logging(func):
+    def warp(request: HttpRequest, *args, **kwargs):
+        if request.session.get('uid', None) or request.COOKIES.get('uid', None):
+            request.session['uid'] = request.COOKIES.get('uid')
+            request.session['username'] = request.COOKIES.get('username')
+            return func(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect('/web/login/')
+
+    return warp
