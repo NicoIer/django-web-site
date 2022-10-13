eeb5993fdd78825da73f0d01f917b23dd8438989
ChenJun <NicoIer@163.com>
2022-10-13 15:53:52 +0800

    添加issue的form表单

---
+++ web/forms/issues.py
@@ -0,0 +1,14 @@
+from django import forms
+from web.forms.bootstrap import BootstrapForm
+from web.models import Issues
+
+
+class IssuesModelForm(BootstrapForm, forms.ModelForm):
+    class Meta:
+        model = Issues
+        exclude = ['project', 'creator', 'start_date', 'end_date']
+        # fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        forms.ModelForm.__init__(self, *args, **kwargs)
+        BootstrapForm.__init__(self, *args, **kwargs)
