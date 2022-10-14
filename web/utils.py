6899174715f73103626244feeee24fa843cc5ad0
ChenJun <NicoIer@163.com>
2022-10-14 18:00:53 +0800

    移动project的校验方法

--- web/utils.py
+++ web/utils.py
@@ -1,4 +1,3 @@
-import asyncio
 import datetime
 import hashlib
 import json
@@ -7,15 +6,13 @@ from datetime import timedelta
 
 import minio.error
 from django.conf import settings
-from django.http import HttpRequest, HttpResponseRedirect, JsonResponse
+from django.http import HttpRequest, HttpResponseRedirect
 from django.http import HttpResponse
-from django.shortcuts import redirect
 from minio import Minio
 from minio.deleteobjects import DeleteObject
 from minio.error import InvalidResponseError
 
 from web import models
-from web.forms.project import ProjectModelForm
 from web.models import User
 
 
@@ -159,61 +156,6 @@ class MinIoManager:
 
 
 minio_manager = MinIoManager()
-
-
-def check_form(form: ProjectModelForm, request: HttpRequest) -> JsonResponse:
-    if form.is_valid():
-        # ToDo 这里可能会有异常 为项目创建一个Bucket
-        _ = models.Project.objects.last()
-        _id = _.id + 1 if _ else 1
-        # toDo 这里可能会有并发异常...
-        bucket_name = '{}-{}'.format(request.tracer.user.id, (_id + 1))
-        location = settings.MINIO_DEFAULT_REGION
-
-        # 创建桶 顺便改变逻辑
-        minio_manager.create_bucket(bucket_name, location)
-        minio_manager.set_bucket_public_read(bucket_name)
-        # 为项目属性赋值
-        form.instance.bucket = bucket_name
-        form.instance.region = location
-
-        # 验证通过
-        form.instance.creator = request.tracer.user
-        # 保存数据到数据库
-        form.save()
-        #
-        return_data = {
-            'status': True,
-            'error': form.errors
-        }
-        return JsonResponse(return_data)
-    else:
-        return_data = {
-            'status': False,
-            'error': form.errors
-        }
-        return JsonResponse(data=return_data)
-
-
-def update_project_star(request: HttpRequest, project_type: str, project_id: int) -> HttpResponse:
-    try:
-        project = models.Project.objects.get(id=project_id, creator=request.tracer.user)
-    except Exception:
-        return HttpResponse("???")
-    remove = project.star
-    project.star = not project.star
-    project.save()
-
-    if project_type == 'my' or project_type == 'star' or (
-            project_type == 'join' and project in request.tracer.user.joined_project.all()):  #
-        if remove:
-            request.tracer.user.stared_project.remove(project_id)
-        else:
-            request.tracer.user.stared_project.add(project_id)
-
-        return redirect('project_list')
-    else:
-        return HttpResponse("?????")
 
 
 def login_cookie_session(request, response, user: User, max_age: int = 3600 * 24):
