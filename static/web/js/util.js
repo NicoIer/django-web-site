cb5a26af6f09e253ad22219c861cbc5b9b1d1aca
ChenJun <NicoIer@163.com>
2022-10-17 19:36:38 +0800

    将表单上传的逻辑函数整合到util.js

--- static/web/js/util.js
+++ static/web/js/util.js
@@ -1,12 +1,35 @@
 function form_check(form_id) {
-        const data = $(form_id).serializeArray();
-        let flag = true
-        $.each(data,function (idx,item) {
-            if(item.value==="")
-            {
-                flag = false;
-                return false;
+    const data = $(form_id).serializeArray();
+    let flag = true
+    $.each(data, function (idx, item) {
+        if (item.value === "") {
+            flag = false;
+            return false;
+        }
+    })
+    return {'data': data, 'flag': flag}
+}
+
+function submitForm(form_id, url, method) {
+    const check_data = form_check(form_id);
+    const form_data = check_data['data'];
+    $.ajax({
+        url: url,
+        method: method,
+        data: form_data,
+        success: function (res) {
+            if (res.status) {
+                location.reload()
+            } else {
+                $.each(res.error, function (key, value) {
+                    const id_ = $('#id_' + key);
+                    setTimeout(function () {
+                        id_.popover('hide')
+                    }, 3000);
+                    id_.attr('data-content', value);
+                    id_.popover('show');
+                })
             }
-        })
-        return {'data':data,'flag':flag}
-    }
+        }
+    })
+}
